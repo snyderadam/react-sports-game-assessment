0e508941f3e452f667cdd07dc03bb87b9f6d420b
Adam Snyder <snyderadam@gmail.com>
2020-08-13 15:15:41 -0400

    third

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Team from '../team'
-import ScoreBoard from '../scoreboard'
+import Team from '../team/Team'
+import ScoreBoard from '../scoreboard/Scoreboard'
 
 
 class Game extends React.Component {
